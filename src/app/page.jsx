d90d11b474aea3cd198efee748d8c79554a95f08
Sheikh Sohan <104677321+sheikhsohan@users.noreply.github.com>
2023-10-18 12:32:16 +0600

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,12 +4,14 @@ import Newsletter from '@/components/Newsletter'
 import Player from '@/components/Player'
 import Albums from '@/components/albums/Albums'
 import Blog from '@/components/blog/Blog'
+import WhatsAppButton from '@/components/button/whatsapp'
 import Events from '@/components/events/Events'
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <WhatsAppButton />
       <Player />
       <Events />
       <Albums />
